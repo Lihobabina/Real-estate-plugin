aa1db79e0db79d8d9e6338b9b626aea28c248588
webcodetest <webcodtest@gmail.com>
2025-05-07 20:14:43 +0300

    change ParseRunner class name and file name

---
+++ ParseRunner.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class ParseRunner {
+
+    public function __construct() {
+    }
+
+    public function fetch_houses_from_api() {
+    }
+
+    private function save_houses($houses) {
+    }
+}
